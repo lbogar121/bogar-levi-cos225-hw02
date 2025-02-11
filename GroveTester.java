3332da3e5e40cdf97838178f865be1d0a01087ed
lbogar121 <lbogar121@gmail.com>
2025-02-11 13:45:03 -0500

    Final

--- GroveTester.java
+++ GroveTester.java
@@ -1,3 +1,17 @@
 public class GroveTester {
-    
+    public static void main(String[]args){
+        Grove grove1=new Grove("grove1");
+        System.out.println(grove1);
+        for(int i=0;i<6;i++){
+            Tree newTree=new Tree(i,37,"Spruce");
+            grove1.plantTree(newTree);
+        }
+        System.out.println(grove1);
+        grove1.removeTree(3);
+        grove1.removeTree(5);
+        System.out.println(grove1);
+        Tree mapleTree=new Tree(7,13,"Maple");
+        grove1.plantTree(mapleTree);
+        System.out.println(grove1);
+    }
 }
